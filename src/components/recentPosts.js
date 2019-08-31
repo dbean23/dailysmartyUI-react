42bd66f9824e32cad4bfef8e5004160fa507cf77
David Bean <dbean23@gmail.com>
2019-08-31 15:48:43 -0600

    created Recent Posts and Search Bar

---
+++ src/components/recentPosts.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class RecentPosts extends Component {
+    render () {
+        return (
+            <div className="recent-posts">
+                <div className="recent-posts__wrapper">
+                    <div className="recent-posts__heading">
+                        <ul className="recent_posts__posts">
+                            <li>recent post 0</li>
+                            <li>recent post 1</li>
+                            <li>recent post 2</li>
+                            <li>recent post 3</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
