ce997ae01ad52da5a4ed353002ec77376fe294a6
David Bean <dbean23@gmail.com>
2019-09-01 15:14:36 -0600

    finished app

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
-export default class Logo extends Component {
-    render () {
+import { Link } from 'react-router-dom';
+
+class Logo extends Component {
+    render() {
+        const size = {
+            height: this.props.size ? this.props.size : 105,
+            width: this.props.size ? this.props.size : 105,
+        }
         return (
             <div className="logo-main">
-                <img alt="dailysmarty ui logo image big" src="/assets/ds_circle_logo.png"/>
+                <Link to="/">
+                    <img style={size} alt="daily smarty ui image logo big" src="/assets/ds_circle_logo.png"/>
+                </Link>
             </div>
-        );
+        )
     }
 }
+
+export default Logo;
